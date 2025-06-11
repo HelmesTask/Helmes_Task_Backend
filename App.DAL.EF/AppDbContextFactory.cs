be6a344888524ca735501db0f99658f7a242c099
alsunj <alex.sunjajev@gmail.com>
2025-06-11 17:41:45 +0300

    switch machines

---
+++ App.DAL.EF/AppDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace App.DAL.EF;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(String[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        return new AppDbContext(optionsBuilder.Options);
+    }
+    
+}
