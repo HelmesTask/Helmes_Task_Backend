8213dc9c6a92fc9c4394dcede138e3cc4cf1fdc7
alsunj <alsunj@taltech.ee>
2025-06-11 19:59:00 +0300

    Create docker container and get initial connection to the ms sql server"

---
+++ App.DAL.EF/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using App.Domain;
+using App.Domain.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace App.DAL.EF;
+
+
+public class AppDbContext : DbContext 
+{
+    public DbSet<Sector> Sectors { get; set; } = default!;
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entity in ChangeTracker.Entries().Where(e => e.State != EntityState.Deleted))
+        {
+            foreach (var prop in entity
+                         .Properties
+                         .Where(x => x.Metadata.ClrType == typeof(DateTime)))
+            {
+                Console.WriteLine(prop);
+                prop.CurrentValue = ((DateTime) prop.CurrentValue).ToUniversalTime();
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ Base.DAL.EF/BaseEntityRepository.cs
@@ -0,0 +1,38 @@
+﻿using Base.Contracts.DAL;
+using Base.Contracts.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Base.DAL.EF;
+
+public class BaseEntityRepository<TDomainEntity, TDalEntity, TDbContext> :
+    BaseEntityRepository<Guid, TDomainEntity, TDalEntity, TDbContext>, IEntityRepository<TDalEntity>
+    where TDomainEntity : class, IDomainEntityId
+    where TDalEntity : class, IDomainEntityId
+    where TDbContext : DbContext
+{
+    public BaseEntityRepository(TDbContext dbContext, IDalMapper<TDomainEntity, TDalEntity> mapper) : base(dbContext,
+        mapper)
+    {
+    }
+
+
+}
+
+public class BaseEntityRepository<TKey, TDomainEntity, TDalEntity, TDbContext>
+    where TKey : IEquatable<TKey>
+    where TDomainEntity : class, IDomainEntityId
+    where TDalEntity : class, IDomainEntityId
+    where TDbContext : DbContext
+
+{
+    protected readonly TDbContext RepoDbContext;
+    protected readonly DbSet<TDomainEntity> RepoDbSet;
+    protected readonly IDalMapper<TDomainEntity, TDalEntity> Mapper;
+
+    public BaseEntityRepository(TDbContext dbContext, IDalMapper<TDomainEntity, TDalEntity> mapper)
+    {
+        RepoDbContext = dbContext;
+        RepoDbSet = RepoDbContext.Set<TDomainEntity>();
+        Mapper = mapper;
+    }
+}
